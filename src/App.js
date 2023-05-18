83340f6bdd8f8a81368984fe392ef457085d719b
Your Name <you@example.com>
2023-05-18 16:19:33 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload the demo-server1.
+          Edit <code>src/App.js</code> and save to reload the prod-server.
         </p>
         <a
           className="App-link"
