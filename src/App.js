260315e542191f533a48c9f3d50d7d53ba6fc631
balavenkateshnp <130744915+balavenkateshnp@users.noreply.github.com>
2023-05-18 16:25:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload the prod-server.
+          Edit <code>src/App.js</code> and save to reload the prod-servers.
         </p>
         <a
           className="App-link"
