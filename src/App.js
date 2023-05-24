6f2afb2e219628d735307e6a7cb030f11c6f0bb1
balavenkateshnp <130744915+balavenkateshnp@users.noreply.github.com>
2023-05-24 17:11:08 +0530

    server-name-changed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
 
-          Edit <code>src/App.js</code> and save to reload the Production-Servers!.
+          Edit <code>src/App.js</code> and save to reload the demo-Servers!.
 
         </p>
         <a
